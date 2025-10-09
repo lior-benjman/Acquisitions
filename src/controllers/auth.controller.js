94eff871af9dd9d9964f313ac6c109beb9805ac3
lior-benjman <lior114452@gmail.com>
2025-10-09 07:54:50 +0300

    Adding utils and routes

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+import logger from "../config/logger.js";
+import {signupSchema} from "../validations/auth.validations.js";
+
+export const signup = async (req, res, next) => {
+    try {
+        const validationResult = signupSchema.safeParse(req.body);
+        if (!validationResult.success) {
+            logger.warn('Validation error during sign-up:', validationResult.error.errors);
+            return res.status(400).json({ error: 'Invalid input', details: validationResult.error.errors });
+        }
+
+        const { name, email, role } = validationResult.data;
+
+        // AUTH SERVICE
+
+        logger.info(`User signed up: ${email}`);
+        res.status(201).send('User signed up successfully');
+
+    } catch (e) {
+        logger.error('Error during sign-up:', e);
+        if(e.message === 'User already exists'){
+            res.status(409).json({ error: 'User already exists' });
+        }
+        next(e);
+    }
+};
---
+++ src/utils/cookies.js
@@ -0,0 +1,13 @@
+export const cookies = {
+    getOptions: () => ({sameSite: 'strict'}),
+
+    set: (res, name, value, options = {}) => {
+        res.cookie(name, value, { ...cookies.getOptions(), ...options });
+    },
+    clear: (res, name, options = {}) => {
+        res.clearCookie(name, { ...cookies.getOptions(), ...options });
+    },
+    get: (req, name) => {
+        return req.cookies[name];
+    }
+};
